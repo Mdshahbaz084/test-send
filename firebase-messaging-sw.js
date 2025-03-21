e2f2376f146b6960b08bc887b9174191e458937d
Md Shahbaz <168327445+Mdshahbaz084@users.noreply.github.com>
2025-03-21 20:54:21 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,25 +1,27 @@
-// firebase-messaging-sw.js
-importScripts('https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/10.0.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/9.1.3/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.1.3/firebase-messaging-compat.js');
 
-firebase.initializeApp({
-    apiKey: "AIzaSyBfqQkLMdirE1s3QDAU-k8kZJDSJ-AHfxI",
-    authDomain: "madrsa-sikariya.firebaseapp.com",
-    projectId: "madrsa-sikariya",
-    storageBucket: "madrsa-sikariya.firebasestorage.app",
-    messagingSenderId: "1066102927865",
-    appId: "1:1066102927865:web:d5e649be4178d363408d28"
-});
+const firebaseConfig = {
+  apiKey: "AIzaSyBfqQkLMdirE1s3QDAU-k8kZJDSJ-AHfxI",
+  authDomain: "madrsa-sikariya.firebaseapp.com",
+  projectId: "madrsa-sikariya",
+  storageBucket: "madrsa-sikariya.firebasestorage.app",
+  messagingSenderId: "1066102927865",
+  appId: "1:1066102927865:web:d5e649be4178d363408d28",
+  measurementId: "G-3S96TVYFJC"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
-    console.log('Received background message:', payload);
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-        icon: payload.notification.icon || '/favicon.ico'
-    };
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/firebase-logo.png'
+  };
 
-    self.registration.showNotification(notificationTitle, notificationOptions);
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
